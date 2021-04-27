9e5ccdd61e77a263c2af5f2b69c11e3d331d7c48
Mats G <ed54res@hotmail.com>
2021-04-27 16:28:22 +0200

    2. Setup Particles

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import fragment from "./shaders/fragment.glsl";
 import vertex from "./shaders/vertex.glsl";
+//let OrbitControls = require("three-orbit-controls")(THREE);
 
 export default class Sketch{
     constructor(options){
@@ -19,18 +20,17 @@ export default class Sketch{
 		this.renderer.setClearColor(0x000000, 1); 
         this.container.appendChild(this.renderer.domElement);
 
-        this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-        this.camera.position.z = 1;    
-        this.scene = new THREE.Scene();    
-        this.addMesh();
+        this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 3000 );
+        this.camera.position.z = 1000;    
+        this.scene = new THREE.Scene();
         this.time = 0;
+        //this.controls = new OrbitControls(this.camera, this.renderer.domElement);    
+        this.addMesh();
+
         this.render();
     }
 
     addMesh(){
-        this.geometry = new THREE.PlaneBufferGeometry( 1,1 );
-		
-        this.material = new THREE.MeshNormalMaterial();
 		this.material = new THREE.ShaderMaterial({
 			fragmentShader:fragment,
 			vertexShader:vertex,
@@ -38,16 +38,30 @@ export default class Sketch{
 				progress: {type: "f", value: 0}
 			},
 			side: THREE.DoubleSide
-		})
+		})        
+        let number = 512*512;
+        //this.geometry = new THREE.PlaneBufferGeometry( 1000,1000, 10, 10);
+        this.geometry = new THREE.BufferGeometry( );
+		this.positions = new THREE.BufferAttribute(new Float32Array(number*3),3);
+
+        let index = 0;
+        for (let i = 0; i < 512; i++) {
+            let posX = i - 256;
+            for (let j = 0; j < 512; j++) {
+                this.positions.setXYZ(index,posX*2,(j-256)*2,0)
+                index++;
+            }            
+        }
+
+        this.geometry.setAttribute("position", this.positions)
         this.mesh = new THREE.Points( this.geometry, this.material );
         this.scene.add( this.mesh );        
     }
 
     render() {
         this.time++;
-        this.mesh.rotation.x += 0.01;
-        this.mesh.rotation.y += 0.02;    
-        //console.log(this.time);
+        //this.mesh.rotation.x += 0.01;
+        //this.mesh.rotation.y += 0.02;    
         this.renderer.render( this.scene, this.camera );
         requestAnimationFrame(this.render.bind(this));
 
--- js/shaders/vertex.glsl
+++ js/shaders/vertex.glsl
@@ -3,6 +3,6 @@ void main() {
   vUv = uv;
 
   vec4 mvPosition = modelViewMatrix * vec4( position, 1. );
-  gl_PointSize = 50. * ( 1. / - mvPosition.z );
+  gl_PointSize = 2000. * ( 1. / - mvPosition.z );
   gl_Position = projectionMatrix * mvPosition;
 }
